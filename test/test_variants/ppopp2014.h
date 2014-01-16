299422af5fa5327558b4ca26984b7bbc3f346a14
Martin Wimmer <wimmer@par.tuwien.ac.at>
2014-01-16 11:55:50 +0100

    added description to ppopp2014 config file

--- test/test_variants/ppopp2014.h
+++ test/test_variants/ppopp2014.h
@@ -14,16 +14,22 @@
 namespace pheet {
 
 #define SSSP_TEST true
-const procs_t sssp_test_cpus[] = {/*1, 2, 3, 5, 10, 20, 40,*/ 80};
-const unsigned int sssp_test_seeds[] = {0, 1, 2, 3, 4/*, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19*/};
+// Run tests with following numbers of CPUs (capped at max cpus)
+const procs_t sssp_test_cpus[] = {1, 2, 3, 5, 10, 20, 40, 80};
+// Run tests with following random seeds for graph generator (to run multiple times, just add more random seeds)
+const unsigned int sssp_test_seeds[] = {0/*, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19*/};
 const GraphBipartitioningProblem sssp_test_problems[] = {
-		// n, p, max_w
+		// nodes, edge probability, maximum edge weight (integer, minimum weight is always 1)
 		{10000, 0.5, 100000000},
 //		{50000, 0.001, 100000000},
 };
+// Which type of graph is used (so far only 0, erdos-renyi random graphs are supported)
 const int sssp_test_types[] = {0};
-const size_t sssp_test_k[] = {32/*, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768*/};
+// Which values for k to use (only for  data structures that support k)
+const size_t sssp_test_k[] = {/*0, 1, 2, 4, 8, 16, 32, 64, 128, 256,*/ 512/*, 1024, 2048, 4096, 8192, 16384, 32768*/};
 
+
+// Configuration for simulator - not used for normal runs
 //#define SSSP_SIM true
 const size_t sssp_sim_k = 1000;
 const size_t sssp_sim_p = 200;
