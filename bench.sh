1866cf9ec200c0ed217193d5c1cf4dc31552815c
Martin Kalany <m.kalany@gmx.at>
2014-06-11 14:44:27 +0200

    bench for 2 cpus as well To get an idea how well the pareto task storage scales

--- bench.sh
+++ bench.sh
@@ -1,5 +1,7 @@
 make release
 echo "n=1, g_2000_13000_3_10000_42"
 ./build/test/msp/bench/msp-bench -r 2 -n 1 --seq --strategy --strategy2 ../mcsp_doc/graphs/g_2000_13000_3_10000_42
+echo "n=2, g_2000_13000_3_10000_42"
+./build/test/msp/bench/msp-bench -r 2 -n 2 --seq --strategy --strategy2 ../mcsp_doc/graphs/g_2000_13000_3_10000_42
 echo "n=4, g_2000_13000_3_10000_42"
 ./build/test/msp/bench/msp-bench -r 2 -n 4 --seq --strategy --strategy2 ../mcsp_doc/graphs/g_2000_13000_3_10000_42
