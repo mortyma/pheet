33610f24cb0b840f259dd918247ff8ae8ae6e724
Martin Kalany <m.kalany@gmx.at>
2014-06-16 17:23:39 +0200

    REF

--- test/msp/bench/bench.py
+++ test/msp/bench/bench.py
@@ -19,8 +19,31 @@ CPUS = [ 1, 2, 3, 4, 5, 6, 7, 8, 10, 15, 16, 20, 25, 30, 32, 35, 40, 45, 50, 55,
 
 MAX_CPUS = 80
 
-if __name__ == '__main__':
+def bench(algorithms, nrcpus, repetitions, inFile, outFile):
+	#make sure only correct algorithms/variants are given
+	for a in algorithms:
+		if a not in ALGORITHMS:
+			parser.error("Invalid algorithm: " + str(a))
+			
+	#make sure only valid cpu counts are given
+	for n in nrcpus:
+		if n < 1 or n > MAX_CPUS:
+			parser.error("Invalid number of cpus: " + str(n))
 	
+	#assemble the argument/option string for ./msp-bench
+	call = ["./build/test/msp/bench/msp-bench"]
+	call += [inFile.name]
+	call += ["-r " + str(repetitions)]
+	for a in algorithms:
+		call += ["--" + a]
+	for n in nrcpus:
+		call += ["-n " + str(n)]
+		
+	#run ./msp-bench  as a subprocess
+	ret = subprocess.check_output(call)
+	outFile.write(ret)
+
+if __name__ == '__main__':
 	#make
 	subprocess.call(["make", "release"])
 	
@@ -33,29 +56,7 @@ if __name__ == '__main__':
 	parser.add_argument('-r', '--repetitions', help = "Number of repetitions", type = int, default = REPETITIONS)
 	parser.add_argument('-n', '--nrcpus', help = "List of cpus counts", nargs = "+", type = int, default = CPUS)
 	args = parser.parse_args()
-
-	#make sure only correct algorithms/variants are given
-	algorithms = args.algorithm
-	for a in algorithms:
-		if a not in ALGORITHMS:
-			parser.error("Invalid algorithm: " + str(a))
-			
-	#make sure only valid cpu counts are given
-	nrcpus = args.nrcpus
-	for n in nrcpus:
-		if n < 1 or n > MAX_CPUS:
-			parser.error("Invalid number of cpus: " + str(n))
 	
-	#assemble the argument/option string for ./msp-bench
-	call = ["./build/test/msp/bench/msp-bench"]
-	call += [args.input.name]
-	call += ["-r " + str(args.repetitions)]
-	for a in algorithms:
-		call += ["--" + a]
-	for n in nrcpus:
-		call += ["-n " + str(n)]
-		
-	#run ./msp-bench  as a subprocess
-	f = args.output
-	ret = subprocess.check_output(call)
-	f.write(ret)
+	bench(args.algorithm, args.nrcpus, args.repetitions, args.input, args.output)
+
+	
