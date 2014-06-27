8e26ddc8a4545c8fad87566dcb53a733720630b1
Martin Kalany <m.kalany@gmx.at>
2014-06-27 15:22:17 +0200

    store benchmark data in separate file for each algorithm

--- test/msp/bench/bench.py
+++ test/msp/bench/bench.py
@@ -3,8 +3,10 @@
 #Note: one may directly execute ./msp-bench. This python script only provides a more convenient user interface.
 
 import argparse
+import os
 import subprocess
 import sys
+
 
 REPETITIONS = 10
 
@@ -19,7 +21,7 @@ CPUS = [ 1, 2, 3, 4, 5, 6, 7, 8, 10, 15, 16, 20, 25, 30, 32, 35, 40, 45, 50, 55,
 
 MAX_CPUS = 80
 
-def bench(algorithms, nrcpus, repetitions, inFile, outFile):
+def bench(algorithms, nrcpus, repetitions, inFile, outDir):
     #make sure only correct algorithms/variants are given
     for a in algorithms:
         if a not in ALGORITHMS:
@@ -34,14 +36,16 @@ def bench(algorithms, nrcpus, repetitions, inFile, outFile):
     call = ["./build/test/msp/bench/msp-bench"]
     call += [inFile.name]
     call += ["-r " + str(repetitions)]
-    for a in algorithms:
-        call += ["--" + a]
+  
     for n in nrcpus:
         call += ["-n " + str(n)]
         
     #run ./msp-bench  as a subprocess
-    ret = subprocess.check_output(call)
-    outFile.write(ret)
+    for a in algorithms:
+        ret = subprocess.check_output(call + ["--" + a])
+        outFile = os.path.join(outDir, a + ".dat")
+        f = open(outFile, 'w')
+        f.write(ret)
 
 if __name__ == '__main__':
     #make
@@ -51,13 +55,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
        description='Benchmarking multi-criteria shortest path with different pheet task storage implementations.')
     parser.add_argument('-i','--input', help = 'Input file name (reuqired)', type = argparse.FileType('r'), required = True)
-    parser.add_argument('-o','--output', help = 'Output file name (default: stdout)', type = argparse.FileType('w'), default = sys.stdout)
-    parser.add_argument('-l','--limit', help = 'Only benchmark sequential and strategy', action='store_true')
+    parser.add_argument('-o','--output', help = 'Output directory. If given, "directory/<algorithm>.dat" will be used to save results for each algorithm given by -a. Otherwise, output is written to stdout', action = 'store')
+    parser.add_argument('-l','--limit', help = 'Benchmark sequential and strategy', action='store_true')
     parser.add_argument('-s', '--small', help = 'Run for 1, 2 and 4 cpus only', action='store_true')
-    parser.add_argument('-a', '--algorithm', help = "List of algorithms/variants to benchmark", nargs = "+", default = ALGORITHMS)
+    parser.add_argument('-a', '--algorithm', help = "List of algorithms/variants to benchmark. Available/default: {sequential, strategy, strategy2}", nargs = "+", default = ALGORITHMS)
     parser.add_argument('-r', '--repetitions', help = "Number of repetitions", type = int, default = REPETITIONS)
     parser.add_argument('-n', '--nrcpus', help = "List of cpus counts", nargs = "+", type = int, default = CPUS)
     args = parser.parse_args()
+    
+    if os.path.isfile(args.output):
+        raise ValueError("-o needs to specify a directory")
+    
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
     
     if args.limit:
         algorithms = ["sequential", "strategy"]
