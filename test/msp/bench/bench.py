142460d60148d0cc3f9d3702c502054affd21e33
Martin Kalany <m.kalany@gmx.at>
2014-06-27 14:46:19 +0200

    tabs to spaces

--- test/msp/bench/bench.py
+++ test/msp/bench/bench.py
@@ -12,62 +12,62 @@ ALGORITHMS = [ 'sequential', 'strategy', 'strategy2' ]
 
 # Number of cpus
 # In the range [1, 80], contains:
-#	- [1-8]
-#	- all powers of 2
-#	- all multiples of 5
+#    - [1-8]
+#    - all powers of 2
+#    - all multiples of 5
 CPUS = [ 1, 2, 3, 4, 5, 6, 7, 8, 10, 15, 16, 20, 25, 30, 32, 35, 40, 45, 50, 55, 60, 64, 65, 70, 75, 80 ]
 
 MAX_CPUS = 80
 
 def bench(algorithms, nrcpus, repetitions, inFile, outFile):
-	#make sure only correct algorithms/variants are given
-	for a in algorithms:
-		if a not in ALGORITHMS:
-			parser.error("Invalid algorithm: " + str(a))
-			
-	#make sure only valid cpu counts are given
-	for n in nrcpus:
-		if n < 1 or n > MAX_CPUS:
-			parser.error("Invalid number of cpus: " + str(n))
-	
-	#assemble the argument/option string for ./msp-bench
-	call = ["./build/test/msp/bench/msp-bench"]
-	call += [inFile.name]
-	call += ["-r " + str(repetitions)]
-	for a in algorithms:
-		call += ["--" + a]
-	for n in nrcpus:
-		call += ["-n " + str(n)]
-		
-	#run ./msp-bench  as a subprocess
-	ret = subprocess.check_output(call)
-	outFile.write(ret)
+    #make sure only correct algorithms/variants are given
+    for a in algorithms:
+        if a not in ALGORITHMS:
+            parser.error("Invalid algorithm: " + str(a))
+            
+    #make sure only valid cpu counts are given
+    for n in nrcpus:
+        if n < 1 or n > MAX_CPUS:
+            parser.error("Invalid number of cpus: " + str(n))
+    
+    #assemble the argument/option string for ./msp-bench
+    call = ["./build/test/msp/bench/msp-bench"]
+    call += [inFile.name]
+    call += ["-r " + str(repetitions)]
+    for a in algorithms:
+        call += ["--" + a]
+    for n in nrcpus:
+        call += ["-n " + str(n)]
+        
+    #run ./msp-bench  as a subprocess
+    ret = subprocess.check_output(call)
+    outFile.write(ret)
 
 if __name__ == '__main__':
-	#make
-	subprocess.call(["make", "release"])
-	
-	#parse arguments
-	parser = argparse.ArgumentParser(
-	   description='Benchmarking multi-criteria shortest path with different pheet task storage implementations.')
-	parser.add_argument('-i','--input', help = 'Input file name (reuqired)', type = argparse.FileType('r'), required = True)
-	parser.add_argument('-o','--output', help = 'Output file name (default: stdout)', type = argparse.FileType('w'), default = sys.stdout)
-	parser.add_argument('-l','--limit', help = 'Only benchmark sequential and strategy', action='store_true')
-	parser.add_argument('-s', '--small', help = 'Run for 1, 2 and 4 cpus only', action='store_true')
-	parser.add_argument('-a', '--algorithm', help = "List of algorithms/variants to benchmark", nargs = "+", default = ALGORITHMS)
-	parser.add_argument('-r', '--repetitions', help = "Number of repetitions", type = int, default = REPETITIONS)
-	parser.add_argument('-n', '--nrcpus', help = "List of cpus counts", nargs = "+", type = int, default = CPUS)
-	args = parser.parse_args()
-	
-	if args.limit:
-		algorithms = ["sequential", "strategy"]
-	else:
-		algorithms = args.algorithm
-	if args.small:
-		nrcpus = [1,2,3,4]
-	else:
-		nrcpus = args.nrcpus
-	
-	bench(algorithms, nrcpus, args.repetitions, args.input, args.output)
+    #make
+    subprocess.call(["make", "release"])
+    
+    #parse arguments
+    parser = argparse.ArgumentParser(
+       description='Benchmarking multi-criteria shortest path with different pheet task storage implementations.')
+    parser.add_argument('-i','--input', help = 'Input file name (reuqired)', type = argparse.FileType('r'), required = True)
+    parser.add_argument('-o','--output', help = 'Output file name (default: stdout)', type = argparse.FileType('w'), default = sys.stdout)
+    parser.add_argument('-l','--limit', help = 'Only benchmark sequential and strategy', action='store_true')
+    parser.add_argument('-s', '--small', help = 'Run for 1, 2 and 4 cpus only', action='store_true')
+    parser.add_argument('-a', '--algorithm', help = "List of algorithms/variants to benchmark", nargs = "+", default = ALGORITHMS)
+    parser.add_argument('-r', '--repetitions', help = "Number of repetitions", type = int, default = REPETITIONS)
+    parser.add_argument('-n', '--nrcpus', help = "List of cpus counts", nargs = "+", type = int, default = CPUS)
+    args = parser.parse_args()
+    
+    if args.limit:
+        algorithms = ["sequential", "strategy"]
+    else:
+        algorithms = args.algorithm
+    if args.small:
+        nrcpus = [1,2,3,4]
+    else:
+        nrcpus = args.nrcpus
+    
+    bench(algorithms, nrcpus, args.repetitions, args.input, args.output)
 
-	
+    
